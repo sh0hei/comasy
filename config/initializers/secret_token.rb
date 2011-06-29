a40b315e226a723e80174c8e2fc1fd1db2580dec
sh0u <vividbit@gmail.com>
2011-06-29 23:11:33 +0900

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comasy::Application.config.secret_token = '51734dd776025b28831f39d901b334c9c405ff369c37f7e46f5520a2707ebe97a0e17c059d675a2a2f93a94b90c8545a35b27f8af53d6841814711cc68339809'
